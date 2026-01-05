e811b0c651b602d8f8dbd47b5b6c9475c230044a
吴泽 <1026060507@qq.com>
2026-01-05 17:47:57 +0800

    0

--- script_device1.sh
+++ script_device1.sh
@@ -4,5 +4,11 @@
 #######      ./script_device1.sh
 #######      device1 is 2080ti22g
 
-yolo detect train data=VisDrone.yaml model=yolo11n-CAAM.yaml epochs=200 batch=16 imgsz=640 device=1 name='yolo11n-CAAM-VD200-d1'
-yolo detect train data=VisDrone.yaml model=yolo11n-SSAM.yaml epochs=200 batch=16 imgsz=640 device=1 name='yolo11n-SSAM-VD200-d1'
+# yolo detect train data=VisDrone.yaml model=yolo11n-CAAM.yaml epochs=200 batch=16 imgsz=640 device=1 name='yolo11n-CAAM-VD200-d1'
+# yolo detect train data=VisDrone.yaml model=yolo11n-SSAM.yaml epochs=200 batch=16 imgsz=640 device=1 name='yolo11n-SSAM-VD200-d1'
+
+yolo detect train data=VisDrone.yaml model=yolo11n.yaml epochs=200 batch=16 imgsz=640 device=1 name='v11n_CIoU-VD200-d1'
+yolo detect train data=VisDrone.yaml model=yolo11s.yaml epochs=200 batch=16 imgsz=640 device=1 name='v11s_CIoU-VD200-d1'
+yolo detect train data=VisDrone.yaml model=yolo11m.yaml epochs=200 batch=8 imgsz=640 device=1 name='v11m_CIoU-VD200-d1'
+yolo detect train data=VisDrone.yaml model=yolo11l.yaml epochs=200 batch=4 imgsz=640 device=1 name='v11l_CIoU-VD200-d1'
+yolo detect train data=VisDrone.yaml model=yolo11x.yaml epochs=200 batch=4 imgsz=640 device=1 name='v11x_CIoU-VD200-d1'
