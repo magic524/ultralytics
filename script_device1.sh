c0987dc1e5493a3301066c03147bc340a2198ead
吴泽 <1026060507@qq.com>
2026-01-09 15:31:31 +0800

    1

--- script_device1.sh
+++ script_device1.sh
@@ -9,6 +9,9 @@
 
 # Train CAAMv3 by default; keep CAAMv1 and CAAMv2 as references
 # yolo detect train data=VisDrone.yaml model=yolo11n-CAAMv1.yaml epochs=200 batch=16 imgsz=640 device=1 name='yolo11n-CAAMv1-VD200-d1'
-yolo detect train data=VisDrone.yaml model=yolo11n-CAAMv2.yaml epochs=200 batch=16 imgsz=640 device=1 name='yolo11n-CAAMv2-VD200-d1'
-yolo detect train data=VisDrone.yaml model=yolo11n-CAAMv3.yaml epochs=200 batch=16 imgsz=640 device=1 name='yolo11n-CAAMv3-VD200-d1'
-#yolo detect train data=VisDrone.yaml model=yolo11n-SSAMv1.yaml epochs=200 batch=16 imgsz=640 device=1 name='yolo11n-SSAMv1-VD200-d1'
+# yolo detect train data=VisDrone.yaml model=yolo11n-CAAMv2.yaml epochs=200 batch=16 imgsz=640 device=1 name='yolo11n-CAAMv2-VD200-d1'
+# yolo detect train data=VisDrone.yaml model=yolo11n-CAAMv3.yaml epochs=200 batch=16 imgsz=640 device=1 name='yolo11n-CAAMv3-VD200-d1'
+# yolo detect train data=VisDrone.yaml model=yolo11n-SSAMv1.yaml epochs=200 batch=16 imgsz=640 device=1 name='yolo11n-SSAMv1-VD200-d1'
+
+yolo detect train data=VisDrone.yaml model=yolo11n-CAAMv2-v2.yaml epochs=200 batch=16 imgsz=640 device=1 name='yolo11n-CAAMv2-v2-VD200-d1'
+yolo detect train data=VisDrone.yaml model=yolo11s-CAAM-v2.yaml epochs=200 batch=8 imgsz=640 device=1 name='yolo11s-CAAM-v2-VD200-d1'
