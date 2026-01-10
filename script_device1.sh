60bad5f140735964152f7a454b1fdd1416bf046b
吴泽 <1026060507@qq.com>
2026-01-10 18:06:30 +0800

    architecture ablation: remove P5 head and backbone, reduce channels in P4 and P3

--- script_device1.sh
+++ script_device1.sh
@@ -4,18 +4,10 @@
 #######      ./script_device1.sh
 #######      device1 is 2080ti22g
 
-#yolo detect train data=VisDrone.yaml model=yolo11n-CAAM.yaml epochs=200 batch=16 imgsz=640 device=1 name='yolo11n-CAAM-VD200-d1'
-#yolo detect train data=VisDrone.yaml model=yolo11n-SSAM.yaml epochs=200 batch=16 imgsz=640 device=1 name='yolo11n-SSAM-VD200-d1'
-
-# Train CAAMv3 by default; keep CAAMv1 and CAAMv2 as references
-# yolo detect train data=VisDrone.yaml model=yolo11n-CAAMv1.yaml epochs=200 batch=16 imgsz=640 device=1 name='yolo11n-CAAMv1-VD200-d1'
-# yolo detect train data=VisDrone.yaml model=yolo11n-CAAMv2.yaml epochs=200 batch=16 imgsz=640 device=1 name='yolo11n-CAAMv2-VD200-d1'
-# yolo detect train data=VisDrone.yaml model=yolo11n-CAAMv3.yaml epochs=200 batch=16 imgsz=640 device=1 name='yolo11n-CAAMv3-VD200-d1'
-# yolo detect train data=VisDrone.yaml model=yolo11n-SSAMv1.yaml epochs=200 batch=16 imgsz=640 device=1 name='yolo11n-SSAMv1-VD200-d1'
-
-# yolo detect train data=VisDrone.yaml model=yolo11n-CAAMv2-v2.yaml epochs=200 batch=16 imgsz=640 device=1 name='yolo11n-CAAMv2-v2-VD200-d1'
-# yolo detect train data=VisDrone.yaml model=yolo11s-CAAM-v2.yaml epochs=200 batch=8 imgsz=640 device=1 name='yolo11s-CAAM-v2-VD200-d1'
-
-#yolo detect train data=VisDrone.yaml model=yolo11-SPDConv_ECA.yaml epochs=2 batch=16 imgsz=640 device=1 证明非确定性不仅仅是nn.AdaptiveAvgPool2d的原因
-# yolo detect train data=VisDrone.yaml model=yolo11n.yaml epochs=200 batch=16 imgsz=640 device=1 name='yolo11n-VD200-d1-260109'
-# yolo detect train data=VisDrone.yaml model=yolo11n-CAAMv2-v2.yaml epochs=200 batch=16 imgsz=640 device=1 name='yolo11n-CAAMv2-v2-VD200-d1'
+###260110 小目标中yolo架构消融
+yolo detect train data=VisDrone.yaml model=yolo11n-noP5_v1.yaml epochs=200 batch=16 imgsz=640 device=1 name='yolo11n-noP5_v1-VD200-d1'
+# YOLO11n-noP5_v1 summary: 146 layers, 1,851,556 parameters, 1,851,540 gradients, 5.9 GFLOPs
+yolo detect train data=VisDrone.yaml model=yolo11n-noP5_v2.yaml epochs=200 batch=16 imgsz=640 device=1 name='yolo11n-noP5_v2-VD200-d1'
+# YOLO11n-noP5_v2 summary: 126 layers, 642,708 parameters, 642,692 gradients, 8.3 GFLOPs
+yolo detect train data=VisDrone.yaml model=yolo11n-noP5_v3.yaml epochs=200 batch=16 imgsz=640 device=1 name='yolo11n-noP5_v3-VD200-d1'
+# YOLO11n-noP5_v3 summary: 161 layers, 897,278 parameters, 897,262 gradients, 9.1 GFLOPs
